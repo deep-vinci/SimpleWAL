2c66d3460391346bcc85eb8f833ac7c643310e59
deep-vinci <deepakworkpc@gmail.com>
2026-02-22 16:03:06 +0530

    feat: add repl style cli

--- src/main/java/com/myechodb/store/LogManager.java
+++ src/main/java/com/myechodb/store/LogManager.java
@@ -6,26 +6,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
 
 class LogManager {
 
     public static void main(String[] args) {
-        String rawKV = "val:10";
-        long checksum = ChecksumCR32.calculateChecksum(rawKV);
-        String finalLine = rawKV + ":" + checksum + "\n";
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("> ");
+            String rawKV = sc.nextLine();
 
-        Path logFile = Paths.get("data", "log.txt");
-        try {
-            Files.write(
-                logFile,
-                finalLine.getBytes(StandardCharsets.UTF_8),
-                StandardOpenOption.CREATE,
-                StandardOpenOption.APPEND,
-                StandardOpenOption.SYNC
-            );
-            System.out.println("Data written to log file");
-        } catch (IOException e) {
-            e.printStackTrace();
+            long checksum = ChecksumCR32.calculateChecksum(rawKV);
+            String finalLine = rawKV + ":" + checksum + "\n";
+
+            Path logFile = Paths.get("data", "log.txt");
+            try {
+                Files.write(
+                    logFile,
+                    finalLine.getBytes(StandardCharsets.UTF_8),
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND,
+                    StandardOpenOption.SYNC
+                );
+                System.out.println("Data written to log file");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
