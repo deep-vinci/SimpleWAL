6196aff67de3842d5e0a1cec27e826f3450bd1b0
deep-vinci <deepakworkpc@gmail.com>
2026-03-01 18:19:28 +0530

    implement memtable limit check, flush to sstable_x.dat file

--- src/main/java/com/myechodb/store/MemTableLimit.java
+++ src/main/java/com/myechodb/store/MemTableLimit.java
@@ -1,22 +1,65 @@
 package com.myechodb.store;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
 import java.util.TreeMap;
+import java.util.stream.Stream;
 
 public class MemTableLimit {
 
-    public static boolean limitReached(TreeMap<String, String> memTable) {
-        if (memTable.size() > 10) {
-            File folder = new File("./data/db");
+    public static void limitReached(TreeMap<String, String> memTable) throws IOException {
 
-            if (folder.exists() && folder.listFiles() != null) {
-                File[] listOfFiles = folder.listFiles();
-                for (File file : listOfFiles) {
-                    System.out.println(file.getName());
+        if (memTable.size() <= 10) {
+            return;
+        }
+
+        Path dbFolder = Path.of("./data/db");
+
+        if (Files.notExists(dbFolder)) {
+            Files.createDirectories(dbFolder);
+        }
+
+        int maxFileIndex = 0;
+
+        try (Stream<Path> files = Files.list(dbFolder)) {
+            for (Path file : files.toList()) {
+
+                String fileName = file.getFileName().toString();
+
+                if (fileName.startsWith("sstable_") && fileName.endsWith(".dat")) {
+
+                    String indexPart = fileName
+                            .replace("sstable_", "")
+                            .replace(".dat", "");
+
+                    int currentIndex = Integer.parseInt(indexPart);
+                    maxFileIndex = Math.max(maxFileIndex, currentIndex);
                 }
             }
-            return true;
         }
-        return false;
+
+        int newIndex = maxFileIndex + 1;
+        Path newFile = dbFolder.resolve("sstable_" + newIndex + ".dat");
+
+        Files.createFile(newFile);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (var entry : memTable.entrySet()) {
+            sb.append(entry.getKey())
+                    .append("=")
+                    .append(entry.getValue())
+                    .append("\n");
+        }
+
+        Files.writeString(
+                newFile,
+                sb.toString(),
+                StandardOpenOption.WRITE
+        );
+
+        System.out.println("Flushed MemTable to: " + newFile);
+
+        memTable.clear();
     }
 }
