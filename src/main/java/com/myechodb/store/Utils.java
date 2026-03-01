bc29371c0ed9667d400840d3ccf2fca029a5ad65
deep-vinci <deepakworkpc@gmail.com>
2026-03-01 21:26:37 +0530

    chore: decouple file write for now

---
+++ src/main/java/com/myechodb/store/Utils.java
@@ -0,0 +1,27 @@
+package com.myechodb.store;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Utils {
+    static void writeToLog(Path path, String data) {
+        Path fileName = Paths.get(path.toUri());
+        try {
+            Files.writeString(
+                    fileName,
+                    data,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND,
+                    StandardOpenOption.SYNC
+            );
+            System.out.println("Data written to log file");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
