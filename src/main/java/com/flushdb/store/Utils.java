e919f778aeb5ffc8d0849b57e216ccf6096ac9f1
deep-vinci <deepakworkpc@gmail.com>
2026-03-02 00:31:16 +0530

    feat: add max file counter

--- src/main/java/com/flushdb/store/Utils.java
+++ src/main/java/com/flushdb/store/Utils.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.stream.Stream;
 
 public class Utils {
     static void writeToLog(Path path, String data) {
@@ -23,5 +24,29 @@ public class Utils {
             e.printStackTrace();
         }
 
+    }
+
+
+    static int maxFileIndexCounter(Path path) throws IOException {
+        int maxFileIndex = 0;
+
+        try (Stream<Path> files = Files.list(path)) {
+
+            for (Path file : files.toList()) {
+
+                String fileName = file.getFileName().toString();
+
+                if (fileName.startsWith("sstable_") && fileName.endsWith(".dat")) {
+
+                    String indexPart = fileName
+                            .replace("sstable_", "")
+                            .replace(".dat", "");
+
+                    int currentIndex = Integer.parseInt(indexPart);
+                    maxFileIndex = Math.max(maxFileIndex, currentIndex);
+                }
+            }
+        }
+        return maxFileIndex;
     }
 }
